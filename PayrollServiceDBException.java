0d8d9b613454e67149ff240f219fc57af4a940a3
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-11-08 21:06:20 +0530

    Welcome to Employee Payroll using multi-threading

---
+++ PayrollServiceDBException.java
@@ -0,0 +1,9 @@
+package com.bl.jdbcassignment;
+
+@SuppressWarnings("serial")
+public class PayrollServiceDBException extends Exception {
+	public PayrollServiceDBException (String message) {
+		super(message);
+	}
+
+}
